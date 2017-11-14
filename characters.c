bf84a025c16525ceb3a9656ba746201242d1912e
luthierycosta <luthierycosta2010@gmail.com>
2017-11-14 13:47:42 -0200

    aloca personagens

--- characters.c
+++ characters.c
@@ -9,21 +9,25 @@ void aloca_personagens(cList* list) {
 
 	FILE* arquivo = fopen(personagens.txt, "r");
 
-	char* name, house;
+	char* name = (char*) malloc(sizeof(char) * STR_SIZE);
+	char* house = (char*) malloc(sizeof(char) * STR_SIZE);
+
 	int agility, strength, intelligence, health;
 
-	while (arquivo != EOF) { 	//essa condição tá certa?
+	while (fscanf(arquivo, "%[^,], ", name) 		+
+		fscanf(arquivo, "%[^,], ", house) 		+
+		fscanf(arquivo, "%d, ", &agility) 		+
+		fscanf(arquivo, "%d, ", &strength) 		+
+		fscanf(arquivo, "%d, ", &intelligence) 	+
+		fscanf(arquivo, "%d", &health) 		   == 6) {
 
-		fscanf(arquivo, "%[^,], ", name);
-		fscanf(arquivo, "%[^,], ", house);
-		fscanf(arquivo, "%d, ", &agility);
-		fscanf(arquivo, "%d, ", &strength);
-		fscanf(arquivo, "%d, ", &intelligence);
-		fscanf(arquivo, "%d", &health);
-		
-		cPush(list, character_create(name, house, agility, strength, intelligence, health));
-	}
-
+			cPush(list, character_create(name, house, agility, strength, intelligence, health));
+		}
+			/*
+			fclose(arquivo);
+			printf("\nERRO: erro na leitura de personagens.txt");
+			exit(-2);
+			*/
 	fclose(arquivo);
 
 }
@@ -31,6 +35,9 @@ void aloca_personagens(cList* list) {
 Character* character_create(char* _name, char* _house, int _agility, int _strength, int _intelligence, int _health) {
 
 	Character* personagem = (Character*) malloc(sizeof(Character));
+
+	personagem->name = (char*) malloc(sizeof(char) * STR_SIZE);
+	personagem->house = (char*) malloc(sizeof(char) * STR_SIZE);
 
 	strcpy(personagem->name, _name);
 	strcpy(personagem->house, _house);
