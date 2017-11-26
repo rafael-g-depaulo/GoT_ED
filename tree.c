3836acdaeedfaa09df33e99b3e5ec31d3a5f2225
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2017-11-26 20:00:06 -0200

    desenvolvidas add_level(), create_node() e create_tree(). listinha ainda não atualizada

--- tree.c
+++ tree.c
@@ -1,1 +1,42 @@
 #include "tree.h"
+
+/*-----------------------------------------------------------------------------*/
+t_node* node_create() {
+    t_node* node = (t_node*) malloc(sizeof(t_node));
+
+    if (!node) {
+        printf("\nERRO: erro na alocacao de memoria em node_create()...");
+        exit(-1);
+    }
+
+    node->character = NULL;
+    node->left      = NULL;
+    node->right     = NULL;
+
+    return node;
+}
+
+/*-----------------------------------------------------------------------------*/
+void add_level(t_node* tree) {
+
+    if (!(tree->left || tree->right)) {
+        tree->left = node_create();
+        tree->right = node_create();
+    } else {
+        add_level(tree->left);
+        add_level(tree->right);
+    }
+}
+
+/*-----------------------------------------------------------------------------*/
+t_node* tree_create() {
+
+    t_node* tree = node_create();
+
+    int i;
+    for (i = 0; i < 4; i++) {
+        add_level(tree);
+    }
+
+    return tree;
+}
--- tree.h
+++ tree.h
@@ -9,9 +9,32 @@ typedef struct node {
     struct node* right;
 } t_node;
 
-/** EM DESENVOLVIMENTO ***************************/
+/**
+ * @brief cria um nó da arvore e retorna ele
+ * 
+ *  cria um t_node, seta seus membros pra NULL, e retorna.
+ * 
+ * @return t_node* endereço do nó.
+ */
 t_node* node_create();
+
+/**
+ * @brief adiciona um nivel na arvore
+ * 
+ *  passa pela arvore, adicionando dois filhos em cada folha.
+ * 
+ * @param tree endereco da arvore;
+ */
+void add_level(t_node* tree);
+
+/**
+ * @brief cria uma arvore binária vazia completa de 4 níveis
+ * 
+ * @return t_node* endereco da arvore
+ */
 t_node* tree_create();
+
+/** EM DESENVOLVIMENTO ***************************/
 void tree_free();
 void tree_print_preorder();
 /** EM DESENVOLVIMENTO ***************************/
